59cc6fa24383204921b105eaf037c2d9f716956b
LsTheJunior <90662061+LsTheJunior@users.noreply.github.com>
2021-11-15 00:04:36 -0300

    Update API_REST.py

--- API_REST.py
+++ API_REST.py
@@ -1,23 +1,26 @@
 
-from flask import Flask
+from flask import Flask, jsonify, request
 from pydantic import BaseModel
 from flask_pydantic_spec import FlaskPydanticSpec, Response, Request
-from werkzeug.wrappers import request
+from tinydb import TinyDB, database, Query
+from typing import Optional
 
 
 server = Flask(__name__)
 spec = FlaskPydanticSpec('flask', title='API teste')
 spec.register(server)
+database = TinyDB('database.json')
 
 class pessoa(BaseModel):
-    id: int
+    id: Optional[int]
     nome: str
     idade: str
 
 @server.get('/pessoas')
 #@spec.validate(resp=Response(HTTP_200=pessoa))
-def pegar_pessoas():
-    return 'Programaticamente Falando'
+def buscar_pessoas():
+    """Retorna todas as pessoas da base de dados"""
+    return jsonify(database.all())
 
 @server.post('/pessoas') 
 @spec.validate(body=Request(pessoa), resp=Response(HTTP_200=pessoa))  
